7f6af56177d45752aab03e84f3b097d1e8538ca0
Mayank Jha <mayankjha7722@gmail.com>
2018-04-06 20:28:30 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
     production: true,
     firebaseConfig: {
-      apiKey: "AIzaSyAOoZwvsB7-hIiuSUlTB1bKaeLTD4KxJ5s",
-    authDomain: "sih2018-5bfa4.firebaseapp.com",
-    databaseURL: "https://sih2018-5bfa4.firebaseio.com",
-    projectId: "sih2018-5bfa4",
-    storageBucket: "sih2018-5bfa4.appspot.com",
-    messagingSenderId: "517832533340"
+      //your firebase config files
     }
 };
